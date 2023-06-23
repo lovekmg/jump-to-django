fcf122d52e2dbc91535b6ff477442258d07d8d79
lovekmg <rlaaudruakr@gmail.com>
2023-06-23 12:00:02 +0900

    어드민 생성 및 질문(Question), 답변(Answer) 모델 추가

--- config/settings.py
+++ config/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['192.168.0.3', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    'pybo.apps.PyboConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
