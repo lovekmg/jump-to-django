6ace6eb76391529fb6d1b92a7510aece9ee92d37
lovekmg <rlaaudruakr@gmail.com>
2023-06-26 15:52:02 +0900

    공용 앱 생성(like login, logout)

--- config/settings.py
+++ config/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['192.168.0.3', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    'common.apps.CommonConfig',
     'pybo.apps.PyboConfig',
     'django.contrib.admin',
     'django.contrib.auth',
