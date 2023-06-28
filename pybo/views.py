23a7ec74971fd5baef619f574b6d763f55cddbab
lovekmg <rlaaudruakr@gmail.com>
2023-06-28 14:03:49 +0900

    답변 수정 기능 추가

--- pybo/views.py
+++ pybo/views.py
@@ -1,7 +1,7 @@
 # Create your views here.
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
-from .models import Question
+from .models import Question, Answer
 from .forms import QuestionForm, AnswerForm
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
@@ -80,3 +80,21 @@ def question_delete(request, question_id):
         return redirect('pybo:detail', question_id=question.id)
     question.delete()
     return redirect('pybo:index')
+
+@login_required(login_url='common:login')
+def answer_modify(request, answer_id):
+    answer = get_object_or_404(Answer, pk=answer_id)
+    if request.user != answer.author:
+        messages.error(request, '수정권한이 없습니다')
+        return redirect('pybo:detail', question_id=answer.question.id)
+    if request.method == 'POST':
+        form = AnswerForm(request.POST, instance=answer)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.modify_date = timezone.now()
+            answer.save()
+            return redirect('pybo:detail', question_id=answer.question.id)
+    else:
+        form = AnswerForm(instance=answer)
+    context = {'answer': answer, 'form': form}
+    return render(request, 'pybo/answer_form.html', context)
