6ac07a4a8d552ce7bbfa8e6cb61e9b76e9e351cf
lovekmg <rlaaudruakr@gmail.com>
2023-06-28 17:37:17 +0900

    질문 추천 기능 추가

--- pybo/urls.py
+++ pybo/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('question/create/', question_views.question_create, name='question_create'),
     path('question/modify/<int:question_id>', question_views.question_modify, name='question_modify'),
     path('question/delete/<int:question_id>', question_views.question_delete, name='question_delete'),
+    path('question/vote/<int:question_id>', question_views.question_vote, name='question_vote'),
 
     path('answer/create/<int:question_id>/', answer_views.answer_create, name='answer_create'),
     path('answer/modify/<int:answer_id>/', answer_views.answer_modify, name='answer_modify'),
